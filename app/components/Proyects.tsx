8aa0a10f5148f61319c21cae0b87b51957c398b5
Santiago Nahuel Ruiz Sugliani <sruizs@fi.uba.ar>
2024-08-15 21:19:50 -0300

    Cambio altura de los scrolls por bug en teléfonos

--- app/components/Proyects.tsx
+++ app/components/Proyects.tsx
@@ -11,11 +11,13 @@ export default function Proyects() {
             items={projects.filter( (_, idx) => idx % 2 === 0)}
             direction="right"
             speed="slow"
+            className="h-1/2"
             />
             <InfiniteMovingCards
             items={projects.filter( (_, idx) => idx % 2 === 1)}
             direction="left"
             speed="slow"
+            className="h-1/2"
             />
         </section>
     );
